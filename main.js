09bfc23a3941572b9541583f98e6b405f82c9373
GueKubil <kubiilprasetya@gmail.com>
2024-09-20 15:10:21 +0700

    Add files via upload
    Signed-off-by: GueKubil <kubiilprasetya@gmail.com>

--- main.js
+++ main.js
@@ -21,3 +21,20 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   });
 
+
+  
+  
+  
+  
+  function checkScroll() {
+    var scrollPosition = window.scrollY || document.documentElement.scrollTop;
+    var documentHeight = document.documentElement.scrollHeight - window.innerHeight;
+
+    if ((scrollPosition / documentHeight) > 0.35) {
+      document.getElementById("back-animasi").style.opacity = "1"; 
+    } else {
+      document.getElementById("back-animasi").style.opacity = "0";  
+    }
+  }
+
+  window.addEventListener("scroll", checkScroll);
