f2d19b2c261fe9f1429523324ba6d6e11f01d382
GueKubil <kubiilprasetya@gmail.com>
2024-09-22 09:35:56 +0700

    Add files via upload
    Signed-off-by: GueKubil <kubiilprasetya@gmail.com>

--- main.js
+++ main.js
@@ -1,26 +1,35 @@
 // ANIMASI HOVER PADA ICON NAVBAR
 document.addEventListener("DOMContentLoaded", function() {
-    var notif = document.getElementById('notif');
-    var wa = document.getElementById('wa');
-    var fb = document.getElementById('fb');
+    const notif = document.getElementById('notif');
+    const wa = document.getElementById('wa');
+    const fb = document.getElementById('fb');
     
-    wa.addEventListener('mouseover', function() {
-        notif.style.display = 'block';
+   wa.addEventListener('mouseover', function() { // visible
+        notif.style.visibility = 'visible';
+        notif.style.transition = '0.5s';
+        notif.style.opacity = '1';
     });
     
-    wa.addEventListener('mouseout', function() {
-       notif.style.display = 'none'
+    wa.addEventListener('mouseout', function() { // hide
+       notif.style.visibility = 'hidden';
+       notif.style.transition = '1s';
+       notif.style.opacity = '0';
     });
 
-    fb.addEventListener('mouseover', function() {
-        notif.style.display = 'block';
+    fb.addEventListener('mouseover', function() { // hide
+        notif.style.visibility = 'visible';
+        notif.style.transition = '0.5s';
+        notif.style.opacity = '1';
            
-    fb.addEventListener('mouseout', function() {
-         notif.style.display = 'none'
+    fb.addEventListener('mouseout', function() { // visible
+         notif.style.visibility = 'hidden';
+         notif.style.transition = '1s';
+         notif.style.opacity = '0';
         });
     });
   });
 
+  // HIDDEN ANIMASI SEBELUM KONTEN2
 function backAnimasi(scrollPosition, documentHeight) {
     const back_animasi = document.getElementById('back-animasi');
     var scrollPosition = window.scrollY || document.documentElement.scrollTop;
